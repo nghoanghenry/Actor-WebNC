6924cbb2cf6fa0e93a721fdc90d6d58c73657c88
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-10-07 20:00:27 +0700

    Update open api and db

--- actor-service/src/main/java/com/hoang/actorservice/controller/ActorController.java
+++ actor-service/src/main/java/com/hoang/actorservice/controller/ActorController.java
@@ -4,6 +4,8 @@ import com.hoang.actorservice.dto.ActorRequestDTO;
 import com.hoang.actorservice.dto.ActorResponseDTO;
 import com.hoang.actorservice.model.Actor;
 import com.hoang.actorservice.service.ActorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("actors")
+@Tag(name = "Actors", description = "API for managing actors")
 public class ActorController {
     private final ActorService actorService;
     public ActorController(ActorService actorService) {
@@ -18,22 +21,26 @@ public class ActorController {
     }
 
     @GetMapping
+    @Operation(summary = "Get all actors")
     public ResponseEntity<List<ActorResponseDTO>> getAllActors() {
         List<ActorResponseDTO> actorResponseDTOList = actorService.getAllActor();
         return ResponseEntity.ok(actorResponseDTOList);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get actor by id")
     public ResponseEntity<ActorResponseDTO> getActorById(@PathVariable Integer id) {
         return ResponseEntity.ok(actorService.getActorById(id));
     }
 
     @PostMapping
+    @Operation(summary = "Create actor")
     public ResponseEntity<ActorResponseDTO> addActor(@RequestBody ActorRequestDTO actorRequestDTO) {
         return ResponseEntity.ok(actorService.createActor(actorRequestDTO));
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update actor by id")
     public ResponseEntity<ActorResponseDTO> updateActor(@PathVariable Integer id, @RequestBody ActorRequestDTO actorRequestDTO) {
         try {
             return ResponseEntity.ok(actorService.updateActor(id, actorRequestDTO));
@@ -43,6 +50,7 @@ public class ActorController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete actor by id")
     public ResponseEntity<ActorResponseDTO> deleteActor(@PathVariable Integer id) {
         actorService.deleteActor(id);
         return ResponseEntity.ok(actorService.deleteActor(id));
