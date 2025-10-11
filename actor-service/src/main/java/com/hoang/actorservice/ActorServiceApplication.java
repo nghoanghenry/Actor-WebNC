9cdce734da7691b9ef52c6cdff3980b12aa0ae4b
nghoanghenry <121386138+nghoanghenry@users.noreply.github.com>
2025-10-11 20:46:05 +0700

    Update open api and db

---
+++ actor-service/src/main/java/com/hoang/actorservice/ActorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hoang.actorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActorServiceApplication.class, args);
+    }
+
+}
---
+++ actor-service/src/test/java/com/hoang/actorservice/ActorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoang.actorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
